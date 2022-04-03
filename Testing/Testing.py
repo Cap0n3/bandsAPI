2554c1c6050779316df8d2a5ef751320d447d09f
Cap0n3 <cap0n3@protonmail.com>
2022-04-03 19:30:10 +0200

    Improved testing of BankWiki class

--- Testing/Testing.py
+++ Testing/Testing.py
@@ -1,11 +1,29 @@
 import unittest
+import os
+import sys
+import inspect
 from bs4 import BeautifulSoup
 import requests
+import json
+
+# Go to parent folder to find modules (it's so stupid to have to do that ...)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0, parentdir) 
+
 from BandAPI.BandWiki import BandWiki
+
+#================#
+#=== Settings ===#
+#================#
+
+PATH = os.path.dirname(os.path.realpath(__file__))
 
 #===========================#
 #=== Test Data reference ===#
 #===========================#
+
+BAND_LIST = ["Nirvana", "Queens of the stone age", "Knocked Loose", "Korn"]
 
 TEST_MAIN_DICT = {
     'Origin': 'Aberdeen, Washington, U.S.', 
@@ -42,6 +60,28 @@ class test_BandWiki(unittest.TestCase):
     def test_getAllInfos(self):
         resultDict = self.bandDataObj.getAllInfos()
         self.assertDictEqual(resultDict, TEST_MAIN_DICT)
+
+    @unittest.skip("To check dictionnaries, comment this line")
+    def test_OutputDict(self):
+        '''
+        This test method is not regular, I just need it to test outputs for several
+        bands dictionnaries (a sort of parametrization) but without the need of assert.
+
+        If needed, it'll print all output dictionnaries in a file named 'dictToReview.txt' 
+        to review later in order to check if resulting wikipedia scrap is clean or not.
+        '''
+        fileExists = os.path.exists(f"{PATH}/dictToReview.txt")
+        if fileExists :
+            os.remove(f"{PATH}/dictToReview.txt")
+        
+        for band in BAND_LIST:
+            with open(f"{PATH}/dictToReview.txt", "a") as file:
+                wikiObj = BandWiki(band).getAllInfos()
+                prettyDict = json.dumps(wikiObj, indent=4, ensure_ascii=False)
+                file.write(f"### (DICT START) ###\n\n")
+                file.write(f"# ================= {band} ================= #\n\n")
+                file.write(prettyDict)
+                file.write(f"\n\n### (DICT END) ###\n\n")
 
 if __name__ == "__main__":
   unittest.main()
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import json
+import os
 from BandAPI.BandWiki import BandWiki
+
+
+PATH = os.path.dirname(os.path.realpath(__file__))
+
+BAND_LIST = ["Nirvana", "Queens of the stone age", "Knocked Loose", "Korn"]
 
 if __name__ == "__main__":
     wikiObj = BandWiki("Queens of the stone age")
-    # Convert dict to pretty string for viewing (weird characters are due to JSON.dump)
+    # Convert dict to pretty string for viewing
+    # ensure_ascii=False to avoid weird characters are due to JSON.dump
     prettyDict = json.dumps(wikiObj.getAllInfos(), indent=4, ensure_ascii=False)
     print(prettyDict)
-
-    #print(BandWiki("Nirvana").getAllInfos())
