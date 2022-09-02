a3541d205c70e02d3c7fadd70f395cab9c3e1d6a
Cap0n3 <cap0n3@protonmail.com>
2022-09-02 17:28:08 +0200

    Implemented fonctional getTabeBody() method

--- Testing/Test_Wiki_Table/Test_tableHeaderFuncs.py
+++ Testing/Test_Wiki_Table/Test_tableHeaderFuncs.py
@@ -18,7 +18,7 @@ import re
 # For Windows (relative path) 
 dirname = os.path.dirname(__file__)
 # filename = os.path.join(dirname, 'Test_Table_Header/11_tableHeader_case11.html')
-filename = os.path.join(dirname, 'Test_Tables/debugTable_case4.html')
+filename = os.path.join(dirname, 'Test_Tables/debugTable_case7.html')
 # Open html file
 with open(filename, 'r') as htmlTestFile:
     soup = BeautifulSoup(htmlTestFile, "html.parser")
@@ -41,15 +41,15 @@ logger = logging.getLogger(__name__)
 
 # Init & add handler
 stream_handler = logging.StreamHandler(sys.stdout) # To console
-# file_handler = logging.FileHandler('file.log') # To file
-logger.addHandler(stream_handler)
+file_handler = logging.FileHandler(f"{dirname}/ExtractTableLog.log", mode='w') # To file
+logger.addHandler(file_handler)
 
 # Set format of log
 log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-stream_handler.setFormatter(log_format)
+file_handler.setFormatter(log_format)
 
 # Set min log levels I wanna see
-logger.setLevel(logging.WARNING)
+logger.setLevel(logging.DEBUG)
 
 class ExtractTable:
     def __init__(self, allRows):
@@ -130,74 +130,124 @@ class ExtractTable:
         return (rowspan, colspan)
 
     @staticmethod 
-    def insertRows(cell_data, rowSpan, colSpan, row_index, table_repr, firstRow=False):
-        if firstRow:
+    def insertRows(cell_data, rowSpan, colSpan, row_index, table_repr, whichRow):
+        '''
+        This utility method is the core of `getTableHeader()` and `getTableHeader()` methods, it's here to keep code as DRY as possible.
+        It insert cell data (content cleaned of any new line char) in given table list reprentation and take into account if there's rowspan 
+        or colspan in cell attribute. It'll insert n times in same column list if there's a rowspan or insert data in consecutive column list 
+        if there's a colspan. When finished it simply returns table list reprentation.
+
+        Params
+        ------
+        `cell` : `<class 'bs4.element.Tag'>`
+            BS4 cell from table (like `<th rowspan="2">Year</th>`).
+        
+        `rowSpan` : `int`
+            Rowspan attribute of cell (number of rowpsan).
+        
+        `colSpan` : `int`
+            Colspan attribute of cell (number of colpsan).
+
+        `row_index` : `int`
+            Row index of current `cell`.
+
+        `table_repr` : `list`
+            Nested list representing table (ex : [['Year'], ['Album'], ['Label']]).
+
+        `whichRow` : `str`
+            - Select `firstHeaderRow` if this is the very first row of table header.
+            - Select `headerRow` if this is the second (or following) row of table header.
+            - Select `firstBodyRow` if this is the very first row of table body.
+            - Select `bodyRow` if this is the second (or following) row of table body.
+        
+        Returns
+        -------
+        tuple
+            Tuple with rowspan and colspan (return None if there's no rowspan/colspan)
+        '''
+        logger.debug(f"-------------------------------------")
+        logger.debug(f"[START] Entered insertRows() method !")
+        logger.debug(f"[PARAMS] cell : {cell_data}, rowspan/colspan : {rowSpan}/{colSpan}, row index {row_index}, table : {table_repr}, which row : {whichRow}")
+        # Clean cell and convert to text
+        cleanedCell = cell_data.text.replace('\n', '')
+        if whichRow == "firstHeaderRow":
+            logger.debug(f"[*] TEST CELL {cell_data}")
             # === CASE 1 - Normal cell with NO rowspans and NO colspan === #
             if rowSpan == None and colSpan == None:
+                logger.debug(f"[*] CASE 1 - NO ROWSPAN, NO COLSPAN")
                 columnReprList = []
-                columnReprList.append(cell_data.text)
+                columnReprList.append(cleanedCell)
                 table_repr.append(columnReprList)
+                logger.debug(f"[OK] Value '{cleanedCell}' inserted in table list representation !")
             # === CASE 2 - Cell WITH rowspan BUT NO colspan === #
             elif rowSpan != None and colSpan == None:
+                logger.debug(f"[*] CASE 2 - ROWSPAN, NO COLSPAN")
                 columnReprList = []
                 # Insert element n times in column list representation according to rowspan
                 for i in range(rowSpan):
-                    columnReprList.append(cell_data.text)
+                    columnReprList.append(cleanedCell)
                 table_repr.append(columnReprList)
+                logger.debug(f"[OK] Value '{cleanedCell}' inserted {rowSpan} times in table list representation !")
             # === CASE 3 - Cell WITH colspan BUT NO rowspan === #
             elif colSpan != None and rowSpan == None:
+                logger.debug(f"[*] CASE 3 - COLSPAN, NO ROWSPAN")
                 # Create new column list with element n times (depending on colspans)
                 for i in range(colSpan):
                     columnReprList = []
-                    columnReprList.append(cell_data.text)
+                    columnReprList.append(cleanedCell)
                     table_repr.append(columnReprList)
+                    logger.debug(f"[OK] Value '{cleanedCell}' inserted in {colSpan} columns in table list representation !")
             # === CASE 4 - Cell WITH colspan AND rowspan === #
             elif colSpan != None and rowSpan != None:
+                logger.debug(f"\t[*] CASE 4 - COLSPAN, ROWSPAN")
                 # Create new column list with element n times (depending on colspans)
                 for i in range(colSpan):
                     columnReprList = []
                     # Insert element in column list n times (depending on rowspan)
                     for j in range(rowSpan):
-                        columnReprList.append(cell_data.text)
+                        columnReprList.append(cleanedCell)
+                    logger.debug(f"[OK] Value '{cleanedCell}' inserted {rowSpan} times in table list representation !")
                     # Insert column list
                     table_repr.append(columnReprList)
+                    logger.debug(f"[OK] Value '{cleanedCell}' inserted in {colSpan} columns in table list representation !")
             # Return modified list
             return table_repr
-        else:
+        elif whichRow == "headerRow":
             for colIndex, columnList in enumerate(table_repr):
                 # For every case scenario, why try in index exists first
                 try:
                     # -> Check if index exists in column
+                    logger.debug(f"[*] TRY CELL '{cell_data}' FOR COLUMN INDEX {colIndex} AT ROW INDEX {row_index}")
                     columnList[row_index]
                 # If an error occurs we then narrow to exact case scenario
                 except IndexError:
                     # -> If index error then a spot is available for this element
-                    logger.debug(f'[*] COLUMN : {columnList} AT INDEX {row_index}')
-                    logger.debug(f'[*] VALUE TO BE INSERTED : {cell_data.text}')
+                    logger.debug(f"[*] COLUMN : {columnList} AT INDEX {row_index}")
+                    logger.debug(f"[*] VALUE TO BE INSERTED : {cleanedCell}")
                     # === CASE 1 - Normal cell with NO rowspans and NO colspan === #
                     if rowSpan == None and colSpan == None:
-                        logger.debug(f"\t[* CASE 1 *] - NO ROWSPAN, NO COLSPAN")
-                        columnList.insert(row_index, cell_data.text)
-                        logger.debug(f"\tValue '{cell_data.text}' inserted in column {columnList} !")
+                        logger.debug(f"[*] CASE 1 - NO ROWSPAN, NO COLSPAN")
+                        columnList.insert(row_index, cleanedCell)
+                        logger.debug(f"\tValue '{cleanedCell}' inserted in column {columnList} !")
                     # === CASE 2 - Cell WITH rowspan BUT NO colspan === #
                     elif rowSpan != None and colSpan == None:
-                        logger.debug(f"\t[* CASE 2 *] - ROWSPAN, NO COLSPAN")
+                        logger.debug(f"\t[*] CASE 2 - ROWSPAN, NO COLSPAN")
                         for spans in range(rowSpan):
-                            columnList.insert(row_index + rowSpan, cell_data.text)
-                        logger.debug(f"\tValue '{cell_data.text}' inserted {rowSpan} times in column {columnList} !")
+                            columnList.insert(row_index + rowSpan, cleanedCell)
+                        logger.debug(f"\tValue '{cleanedCell}' inserted {rowSpan} times in column {columnList} !")
                     # === CASE 3 - Cell WITH colspan BUT NO rowspan === #
                     elif colSpan != None and rowSpan == None:
-                        logger.debug(f"\t[* CASE 3 *] - COLSPAN, NO ROWSPAN")
-                        columnList.insert(row_index, cell_data.text)
-                        logger.debug(f"\tValue '{cell_data.text}' inserted in column {columnList} !")
-                        table_repr[colIndex + (colSpan - 1)].insert(row_index, cell_data.text)
-                        logger.debug(f"\tValue '{cell_data.text}' inserted in column at index {table_repr[colIndex + (colSpan - 1)]} !")
+                        logger.debug(f"\t[*] CASE 3 - COLSPAN, NO ROWSPAN")
+                        columnList.insert(row_index, cleanedCell)
+                        logger.debug(f"\tValue '{cleanedCell}' inserted in column {columnList} !")
+                        table_repr[colIndex + (colSpan - 1)].insert(row_index, cleanedCell)
+                        logger.debug(f"\tValue '{cleanedCell}' inserted in column at index {table_repr[colIndex + (colSpan - 1)]} !")
                     # === CASE 4 - Cell WITH colspan AND rowspan === #
                     elif colSpan != None and rowSpan != None:
-                        logger.debug(f"\t[* CASE 4 *] - COLSPAN, ROWSPAN")
+                        logger.debug(f"\t[*] CASE 4 - COLSPAN, ROWSPAN")
                         for spans in range(rowSpan):
-                            columnList.insert(row_index + rowSpan, cell_data.text)
-                            table_repr[colIndex + (colSpan - 1)].insert(row_index + rowSpan, cell_data.text)
+                            columnList.insert(row_index + rowSpan, cleanedCell)
+                            table_repr[colIndex + (colSpan - 1)].insert(row_index + rowSpan, cleanedCell)
                     # -> Spot has been found so break loop
                     break
                 else:
@@ -206,6 +256,48 @@ class ExtractTable:
                     continue
             # Return modified list
             return table_repr
+        elif whichRow == "firstBodyRow" :
+            columnList = []
+            # === CASE 1 - Normal cell with NO rowspans === #
+            if rowSpan == None:
+                columnList.append(cleanedCell)
+                table_repr.append(columnList)
+                logger.debug(f"[OK] Value '{cleanedCell}' inserted in table list representation !")
+            # === CASE 2 - Cell WITH rowspan === #
+            elif rowSpan != None:
+                for i in range(rowSpan):
+                    columnList.append(cleanedCell)
+                table_repr.append(columnList)
+                logger.debug(f"[OK] Value '{cleanedCell}' inserted {rowSpan} times in table list representation !")
+            # Return modified list
+            return table_repr
+        elif whichRow == "bodyRow" :
+            for colIndex, columnList in enumerate(table_repr):
+                try:
+                    # -> Check if index exists in column
+                    logger.debug(f"[*] TRY CELL '{cell_data}' FOR COLUMN INDEX {colIndex} AT ROW INDEX {row_index}")
+                    columnList[row_index]
+                # If an error occurs we then narrow to exact case scenario
+                except IndexError:
+                    # === CASE 1 - Normal cell with NO rowspans === #
+                    if rowSpan == None:
+                        table_repr[colIndex].append(cleanedCell)
+                        logger.debug(f"[OK] Value '{cleanedCell}' inserted in table list representation !")
+                    # === CASE 2 - Cell WITH rowspan === #
+                    elif rowSpan != None:
+                        for i in range(rowSpan):
+                            table_repr[colIndex].append(cleanedCell)
+                        logger.debug(f"[OK] Value '{cleanedCell}' inserted {rowSpan} times in table list representation !")
+                    break
+                else:
+                    logger.debug(f"[*] COLUMN {colIndex} AT ROW {row_index} OCCUPIED, GO TO NEXT")
+                    # Index already exists, ok continue searching for a spot in columns (lists)
+                    continue
+            # Return modified list
+            return table_repr        
+        else:
+            logger.warning(f"'{whichRow}' is not a valid argument for 'whichRow' parameter ! It should be either 'firstHeaderRow', 'headerRow' or 'normalRow'.")
+            raise ValueError(f"'{whichRow}' is not a valid argument for 'whichRow' parameter ! \nIt should be either 'firstHeaderRow', 'headerRow', 'firstBodyRow' or 'bodyRow'.")
 
     # ============================== #
     # ========= MAIN FUNCS ========= #
@@ -275,7 +367,7 @@ class ExtractTable:
         elif totalHeaderRows > 0 and titledRow > 0:
             return ("multidimensional", totalHeaderRows, totalColumns)
         else:
-            logger.warning(f'Table type is unknown !')
+            logger.warning(f'Table type is unknown ! Table rows :\n{self.allRows}')
             raise TypeError("Table type is unknown !")
 
     def getTableHeader(self):
@@ -298,6 +390,7 @@ class ExtractTable:
         '''
         # Get header row length
         funcTupleRes = self.getTableType()
+        logger.info(f"[TABLE INFO] Type : {funcTupleRes[0]}, Header length : {funcTupleRes[1]}, Total columns : {funcTupleRes[2]}")
         headerRowLength = funcTupleRes[1]
         # Init table reprentation
         tableRepr = []
@@ -308,7 +401,7 @@ class ExtractTable:
             # Get rowspans and colspans (if any the convert to int or return None)
             rowspan, colspan = ExtractTable.getSpans(cell)
             # Insert data in table reprentation
-            tableRepr = ExtractTable.insertRows(cell, rowspan, colspan, 0, tableRepr, firstRow=True)
+            tableRepr = ExtractTable.insertRows(cell, rowspan, colspan, 0, tableRepr, "firstHeaderRow")
 
         # Log result so far
         logger.info(f"[getTableHeader] TABLE FIRST ROW :\n{tableRepr} (length: {len(tableRepr)})")
@@ -320,16 +413,14 @@ class ExtractTable:
                 # Since we skipped first element, row index is out whack so re-adjust rowIndex at correct index
                 rowIndex += 1
                 # Stop at end of table header
-                #if rowIndex == headerRowLength: break
+                if rowIndex == headerRowLength: break
                 # Get row content and clean them
                 rowChildren = ExtractTable.removeNewLines(row.contents)
                 # Loop through elements and insert them in table list representation
                 for cell in rowChildren:
                     # Get rowspans and colspans (if any the convert to int or return None)
                     rowspan, colspan = ExtractTable.getSpans(cell)
-                    tableRepr = ExtractTable.insertRows(cell, rowspan, colspan, rowIndex, tableRepr)
-                    
-
+                    tableRepr = ExtractTable.insertRows(cell, rowspan, colspan, rowIndex, tableRepr, "headerRow")
         logger.debug(f"[getTableHeader] TABLE FINAL RESULT :\n{tableRepr}")
         return tableRepr
 
@@ -338,23 +429,30 @@ class ExtractTable:
         # Get header row length
         funcTupleRes = self.getTableType()
         headerRowLength = funcTupleRes[1]
-        headerRowIndex = headerRowLength
-        for rowIndex, row in enumerate(self.allRows[headerRowIndex:]):
+        for rowIndex, row in enumerate(self.allRows[headerRowLength:]):
             # Since we skipped header, row index is out whack so re-adjust rowIndex at correct index
-            rowIndex += headerRowIndex
+            #rowIndex += headerRowLength
             # Get row content and clean them
             rowChildren = ExtractTable.removeNewLines(row.contents)
             #Loop through elements and insert them in table list representation
             for cell in rowChildren:
-                print(cell)
                 # Get rowspans and colspans (if any the convert to int or return None)
                 rowspan, colspan = ExtractTable.getSpans(cell)
-                tableBodyRepr = ExtractTable.insertRows(cell, rowspan, colspan, rowIndex, tableBodyRepr)
+                # If this is the first row
+                if rowIndex == 0:
+                    tableBodyRepr = ExtractTable.insertRows(cell, rowspan, colspan, rowIndex, tableBodyRepr, "firstBodyRow")
+                    logger.info(f"Cell entered in first row of table body !")
+                    logger.info(f"{tableBodyRepr}")
+                else:
+                    tableBodyRepr = ExtractTable.insertRows(cell, rowspan, colspan, rowIndex, tableBodyRepr, "bodyRow")
+                    logger.info(f"Cell entered in row of table body !")
+                    logger.info(f"{tableBodyRepr}")
         return tableBodyRepr
 
 
 # ====== UNCOMMENT TO TEST HERE ====== #
 tableObj = ExtractTable(allRows)
-# tableList = tableObj.getTableHeader()
-tableList = tableObj.getTableBody()
-print(tableList)
+tableHeaderList = tableObj.getTableHeader()
+tableBodyList = tableObj.getTableBody()
+#print(tableHeaderList)
+print(tableBodyList)
